42d9f09c573b68d9b001510d4f45859a62a26fe5
ZakiYassine <77936628+ZakiYassine@users.noreply.github.com>
2021-05-23 10:55:12 +0200

    Ajout fonction filter (#1)
    Co-authored-by: zaki <zaki@info1.mlv>

--- src/filter.c
+++ src/filter.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <math.h>
 
 filter *create_filter(int m, int k){
     filter *f = (filter *)malloc(sizeof(filter));
     f->bitArray = create_bitarray(m);
+    f->poids = (int*)malloc(sizeof(int)*k);
+    int i;
+    for(i = 0; i < k; i++){
+        f->poids[i] = rand() % (255-2+1)+2;
+    }
     f->m = m;
     f->k = k;
     return f;
@@ -14,20 +20,39 @@ filter *create_filter(int m, int k){
 
 void free_filter(filter *f){
     free_bitarray(f->bitArray);
+    free(f->poids);
     free(f);
 }
 
 void hash(filter *f, char *str, unsigned hashes[]){
     assert(f != NULL);
-    /*TODO*/
+    int i,j;
+    for(i=0; i< f->k; i++){
+        for(j = 0; str[j] != '\0'; j++){
+            hashes[i] += str[j]*(pow(f->poids[i], strlen(str)-j+1));
+        }
+    }
 }
 
 void add_filter(filter *f, char *str){
     assert(f != NULL);
-
+    unsigned *hashes = (unsigned*)malloc(sizeof(unsigned)*f->k);
+    int i;
+    hash(f, str, hashes);
+    for(i = 0; i < f->k; i++){
+        set_bitarray(f->bitArray, hashes[i]% f->m);
+    }
 }
 
 int is_member_filter(filter *f, char *str){
     assert(f != NULL);
-    
+    unsigned *test = (unsigned*)malloc(sizeof(unsigned)*f->k);
+    int i;
+    hash(f, str, test);
+    for(i = 0; i < f->k; i++){
+        if(get_bitarray(f->bitArray, test[i] % f->m) != 1){
+            return 0;
+        } 
+    }
+    return 1;
 }
